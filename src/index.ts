e2af379c2d4614cdde807c009beca2893e10389e
yanglinshu <nicolasvandarkholme@outlook.com>
2023-07-09 00:36:01 +0800

    feat: svelte to vanilla js cgen

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-export * from './lib/async';
-export * from './lib/number';
+import * as fs from 'node:fs';
+import * as process from 'node:process';
+
+import CodeGenerator from './lib/cgen.js';
+import Parser from './lib/parser.js';
+import SemanticAnalyser from './lib/semantic.js';
+
+const filename = process.argv[2];
+const content = fs.readFileSync(filename, 'utf-8');
+
+const ast = new Parser(content).parse();
+
+const semantic = new SemanticAnalyser(ast).analyze();
+
+const code = new CodeGenerator(ast, semantic).generate();
+
+console.log(code);
---
+++ src/lib/cgen.ts
@@ -0,0 +1,193 @@
+import * as acorn from 'acorn';
+import * as escodegen from 'escodegen';
+import * as estree from 'estree';
+import * as estreewalker from 'estree-walker';
+import * as periscopic from 'periscopic';
+
+import { HTMLAttribute, JSNode, SvelteAST, SvelteElement } from './ast';
+import { SvelteSemantic } from './semantic';
+
+export default class CodeGenerator {
+  private readonly variables: string[] = [];
+  private readonly createStmts: string[] = [];
+  private readonly updateStmts: string[] = [];
+  private readonly destroyStmts: string[] = [];
+  private readonly mangler = new Mangler();
+
+  constructor(
+    private readonly ast: SvelteAST,
+    private readonly semantic: SvelteSemantic
+  ) {}
+
+  public generate(): string {
+    this.ast.html.forEach((node) => this.generateHTML(node, 'target'));
+    this.generateScript();
+    return `
+      export default function () {
+        ${this.variables.map((variable) => `let ${variable};`).join('\n')}
+        ${escodegen.generate(this.ast.script)}
+        const lifecycle = {
+          create(target) {
+            ${this.createStmts.join('\n')}
+          },
+          update(changed) {
+            ${this.updateStmts.join('\n')}
+          },
+          destroy(target) {
+            ${this.destroyStmts.join('\n')}
+          },
+        }
+        return lifecycle;
+      }
+    `;
+  }
+
+  private generateHTML(node: SvelteElement, parentNodeName: string) {
+    switch (node.type) {
+      case 'Element': {
+        const variableName = this.mangler.mangle(node.name as string);
+        this.variables.push(variableName);
+        this.createStmts.push(
+          `${variableName} = document.createElement('${node.name}')`
+        );
+        node.attributes.forEach((attribute) =>
+          this.generateHTMLAttribute(variableName, attribute)
+        );
+        node.children?.forEach((child) =>
+          this.generateHTML(child, variableName)
+        );
+        this.createStmts.push(`${parentNodeName}.appendChild(${variableName})`);
+        this.destroyStmts.push(
+          `${parentNodeName}.removeChild(${variableName})`
+        );
+        break;
+      }
+      case 'Text': {
+        const variableName = this.mangler.mangle('txt');
+        this.variables.push(variableName);
+        this.createStmts.push(
+          `${variableName} = document.createTextNode('${node.text}')`
+        );
+        this.createStmts.push(`${parentNodeName}.appendChild(${variableName})`);
+        this.destroyStmts.push(
+          `${parentNodeName}.removeChild(${variableName})`
+        );
+        break;
+      }
+      case 'Expression': {
+        const variableName = this.mangler.mangle('exp');
+        const expression = escodegen.generate(node.expression as JSNode);
+        const identifiers = periscopic.extract_names(
+          node.expression as estree.Expression
+        );
+        this.variables.push(variableName);
+        this.createStmts.push(
+          `${variableName} = document.createTextNode(${expression})`
+        );
+        this.createStmts.push(`${parentNodeName}.appendChild(${variableName})`);
+        this.destroyStmts.push(
+          `${parentNodeName}.removeChild(${variableName})`
+        );
+        const reactive = [...identifiers].filter((ident) =>
+          this.semantic.reactive.has(ident)
+        );
+        if (reactive.length > 0) {
+          const updateCondition = reactive.slice(1).reduce((acc, ident) => {
+            return `${acc} || changed.includes('${ident}')`;
+          }, `changed.includes('${reactive[0]}')`);
+          this.updateStmts.push(
+            `if (${updateCondition}) ${variableName}.textContent = ${expression}`
+          );
+        }
+        break;
+      }
+    }
+  }
+
+  private generateHTMLAttribute(nodeName: string, attribute: HTMLAttribute) {
+    if (attribute.name.startsWith('on:')) {
+      const eventName = attribute.name.slice(3);
+      const handlerName = escodegen.generate(attribute.value as JSNode);
+      this.createStmts.push(
+        `${nodeName}.addEventListener('${eventName}', ${handlerName})`
+      );
+      this.destroyStmts.push(
+        `${nodeName}.removeEventListener('${eventName}', ${handlerName})`
+      );
+    } else {
+      const value = escodegen.generate(attribute.value as JSNode);
+      this.createStmts.push(
+        `${nodeName}.setAttribute('${attribute.name}', ${value})`
+      );
+      this.updateStmts.push(
+        `${nodeName}.setAttribute('${attribute.name}', ${value})`
+      );
+    }
+  }
+
+  private generateScript() {
+    let currentScope = this.semantic.root;
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const outerThis = this;
+    estreewalker.walk(this.ast.script as unknown as estree.Program, {
+      enter(node) {
+        if (outerThis.semantic.map.has(node)) {
+          currentScope = outerThis.semantic.map.get(node) as periscopic.Scope;
+        }
+        if (node.type === 'UpdateExpression') {
+          const argument = node.argument as estree.Identifier;
+          if (outerThis.shouldLifecycleUpdate(argument, currentScope)) {
+            this.replace(
+              outerThis.generateLifecycleUpdate(node, argument.name)
+            );
+            this.skip();
+          }
+        }
+        if (node.type === 'AssignmentExpression') {
+          const left = node.left as estree.Identifier;
+          if (outerThis.shouldLifecycleUpdate(left, currentScope)) {
+            this.replace(outerThis.generateLifecycleUpdate(node, left.name));
+            this.skip();
+          }
+        }
+      },
+      leave(node) {
+        if (outerThis.semantic.map.has(node)) {
+          currentScope = currentScope.parent as periscopic.Scope;
+        }
+      },
+    });
+  }
+
+  private shouldLifecycleUpdate(
+    node: estree.Identifier,
+    currentScope: periscopic.Scope
+  ): boolean {
+    return (
+      currentScope.find_owner(node.name) === this.semantic.root &&
+      this.semantic.referred.has(node.name)
+    );
+  }
+
+  private generateLifecycleUpdate(node: estree.Expression, name: string) {
+    const lifecycleUpdate = {
+      type: 'SequenceExpression',
+      expressions: [
+        node,
+        acorn.parseExpressionAt(`lifecycle.update(['${name}'])`, 0, {
+          ecmaVersion: 2022,
+        }) as estree.Expression,
+      ],
+    } as estree.Node;
+    return lifecycleUpdate;
+  }
+}
+
+class Mangler {
+  private counter = 0;
+
+  public mangle(name: string): string {
+    const newName = `${name}_${this.counter++}`;
+    return newName;
+  }
+}
